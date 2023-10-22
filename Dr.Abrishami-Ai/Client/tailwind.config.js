b02b8c98723f5876674597e1c084e5b361ca171f
erfan mizban <129388389+Erfuunm@users.noreply.github.com>
2023-10-22 22:33:05 +0330

    salam

---
+++ Dr.Abrishami-Ai/Client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./wwwroot/index.html", "./Pages/**/*.{razor,cshtml}", "./Shared/**/*.{razor,cshtml}" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
