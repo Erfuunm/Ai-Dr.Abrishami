b36e176be6341a1f20cb687832b66ca59e99d158
erfan mizban <129388389+Erfuunm@users.noreply.github.com>
2023-11-02 20:55:18 +0330

    Authentication & Login Page

--- Dr.Abrishami-Ai/Client/Program.cs
+++ Dr.Abrishami-Ai/Client/Program.cs
@@ -1,15 +1,26 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Blazored.LocalStorage;
+
 using Dr.Abrishami_Ai.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Blazored.SessionStorage;
+using Dr.Abrishami_Ai.Client.Extention;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
+builder.Services.AddBlazoredSessionStorageAsSingleton();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationExtention>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
---
+++ Dr.Abrishami-Ai/Server/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Dr.Abrishami_Ai.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dr.Abrishami_Ai.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto login)
+        {
+            SessionDTO sessionDTO = new SessionDTO();
+
+            if (login.Username == "admin" && login.Password == "admin159753")
+            {
+                sessionDTO.Nombre = "admin";
+                sessionDTO.Correo = login.Username;
+                sessionDTO.Rol = "Administrator";
+            }
+            else
+            {
+                sessionDTO.Nombre = "User";
+                sessionDTO.Correo = login.Username;
+                sessionDTO.Rol = "User";
+
+            }
+            return StatusCode(StatusCodes.Status200OK, sessionDTO);
+        }
+    }
+}
