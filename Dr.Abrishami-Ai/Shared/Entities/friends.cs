414c2e6757c8e066d3906b7520837e87defadd84
erfan mizban <129388389+Erfuunm@users.noreply.github.com>
2023-11-17 17:05:07 +0330

    Profile Page and Users Page became Dynamic

---
+++ Dr.Abrishami-Ai/Shared/Entities/friends.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dr.Abrishami_Ai.Shared.Entities
+{
+    public class friends
+
+{
+        public string friendsId { get; set; } 
+
+        public string Name { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public int RankinClass { get; set; }
+
+    }
+}
